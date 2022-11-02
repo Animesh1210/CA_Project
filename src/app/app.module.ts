bd645b0db782ccc8da4417b0260fc8ff8015ac49
Animesh Singh <animeshsingh.mail@gmail.com>
2022-11-03 03:55:22 +0530

    fixed static nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       {path: 'login', component: LoginComponent},
-      {path: 'Register', component: RegisterComponent}
+      {path: 'Register', component: RegisterComponent},
+      {path: 'Home', component: HomeComponent}
     ]),
   ],
   providers: [],
